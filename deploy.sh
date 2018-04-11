99d4a14c90165f6985c9fcfd549c2dfec56fb8f8
rafaelzomer <rafaelzomer@gmail.com>
2018-04-11 20:31:33 -0300

    publish

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
+git checkout gh-pages
 git subtree split --prefix dist -b gh-pages
+git add . 
+git commit -am "publish"
 git push -f origin gh-pages:gh-pages
+git checkout master
 git branch -D gh-pages
