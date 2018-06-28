2487d56d2127ae635a88fd5e81b4cd06c13f6c35
Mateus Generoso <mtsgeneroso@gmail.com>
2018-06-28 20:57:26 -0300

    Added toggle for input type

--- src/app/transforms/rotation.js
+++ src/app/transforms/rotation.js
@@ -1,19 +1,13 @@
 import convert from '../convert';
 
-function _run(x, y, angle){
-  
-  let {r, a} = convert.cartToPolar(x, y);
+function _run(x, y, angle) {
+  let { r, a } = convert.cartToPolar(x, y);
   a += angle;
-  let cart = convert.polarToCart(r ,a);
-  if(a > 90 && a < 180) {
-    if(x < 0 && y > 0 && cart.x > 0 && cart.y < 0){
-      cart.x *= -1;
-      cart.y *= -1;
-    }
-  }
-  return { x: cart.x, y: cart.y}
+  let cart = convert.polarToCart(r, a);
+  console.log(cart.x, cart.y, a)
+  return { x: cart.x, y: cart.y };
 }
 
 export default {
   run: _run
-}
+};
